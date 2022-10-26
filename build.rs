c73f665b4bb831cd747c6b3924b0d49aff075dcc
luojia65 <me@luojia.cc>
2022-10-26 15:52:48 +0800

    lib: build script for generic platform

---
+++ build.rs
@@ -0,0 +1,49 @@
+fn main() {
+    use std::{env, fs, path::PathBuf};
+
+    let ld = &PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("linker.ld");
+    fs::write(ld, LINKER).unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-link-arg=-T{}", ld.display());
+}
+
+const LINKER: &[u8] = br#"
+OUTPUT_ARCH(riscv)
+ENTRY(_start)
+MEMORY {
+    RAM : ORIGIN = 0x80000000, LENGTH = 2M
+}
+REGION_ALIAS("REGION_TEXT", RAM);
+REGION_ALIAS("REGION_RODATA", RAM);
+REGION_ALIAS("REGION_DATA", RAM);
+REGION_ALIAS("REGION_BSS", RAM);
+SECTIONS {
+    .text : ALIGN(4) {
+        *(.text.entry)
+        *(.text .text.*)
+    } > REGION_TEXT
+    .rodata : ALIGN(8) {
+        srodata = .;
+        *(.rodata .rodata.*)
+        *(.srodata .srodata.*)
+        . = ALIGN(8);
+        erodata = .;
+    } > REGION_RODATA
+    .data : ALIGN(8) {
+        sdata = .;
+        *(.data .data.*)
+        *(.sdata .sdata.*)
+        . = ALIGN(8);
+        edata = .;
+    } > REGION_DATA
+    .bss (NOLOAD) : ALIGN(8) {
+        *(.bss.uninit)
+        sbss = .;
+        *(.bss .bss.*)
+        *(.sbss .sbss.*)
+        ebss = .;
+    } > REGION_BSS
+    /DISCARD/ : {
+        *(.eh_frame)
+    }
+}"#;
