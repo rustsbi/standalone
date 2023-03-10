30dd50aad21493c4166786ded3a8ee38d079a1ac
luojia65 <me@luojia.cc>
2023-03-10 14:23:24 +0800

    machine: framework for allwinner-d1

--- machine/src/main.rs
+++ machine/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use rom_rt::entry;
+use rom_rt::{Handover, Parameters};
+
+#[entry]
+fn main(params: Parameters) -> Handover {
+    #[cfg(feature = "allwinner-d1")]
+    {} // todo: use serial
+    Handover::from(params)
+}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
