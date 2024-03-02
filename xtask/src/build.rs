af8ed8a95d18c5db12dedda97f62cbd4ccf88daf
Zhouqi Jiang <luojia@hust.edu.cn>
2024-03-02 15:01:05 +0800

    xtask: build for no specific platform
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -1,5 +1,7 @@
 mod allwinner_d1_series;
 use allwinner_d1_series::{build_allwinner_d1_series, flash_allwinner_d1_series};
+mod no_specific_platform;
+use no_specific_platform::build_no_specific_platform;
 
 use crate::{app::Platform, Config};
 use std::error::Error;
@@ -8,7 +10,7 @@ pub fn build_main(config: &Config) -> Result<(), Box<dyn Error>> {
     match config.platform {
         Platform::AllwinnerD1Series => build_allwinner_d1_series(config),
         Platform::Sophgo2002Series => todo!(),
-        Platform::NoSpecificPlatform => todo!(),
+        Platform::NoSpecificPlatform => build_no_specific_platform(config),
     }
     Ok(())
 }
---
+++ xtask/src/build/no_specific_platform.rs
@@ -0,0 +1,34 @@
+use os_xtask_utils::{BinUtil, Cargo, CommandExt};
+use crate::Config;
+
+const TARGET: &'static str = "riscv64imac-unknown-none-elf";
+
+pub fn build_no_specific_platform(config: &Config) {
+    let features = machine_features_from_config(config);
+    Cargo::build()
+        .package("rustsbi-machine")
+        .features(false, features)
+        .target(TARGET)
+        .release()
+        .invoke();
+    let elf_path = crate::PROJECT
+        .join("target")
+        .join(TARGET)
+        .join("release")
+        .join("rustsbi-machine");
+    let bin_path = elf_path.with_extension("bin");
+    BinUtil::objcopy()
+        .arg("--binary-architecture=riscv64")
+        .arg(elf_path)
+        .args(["--strip-all", "-O", "binary"])
+        .arg(&bin_path)
+        .invoke();
+}
+
+fn machine_features_from_config(config: &Config) -> Vec<&'static str> {
+    let mut ans = Vec::new();
+    if config.machine_fdt_ident_enabled.unwrap_or(true) {
+        ans.push("fdt");
+    }
+    ans
+}
